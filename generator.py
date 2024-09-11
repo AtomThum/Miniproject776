c853e1087a879430953a81d3cd704075007d6763
AtomThum <atom.olee@gmail.com>
2024-09-11 09:41:43 +0700

    Reorganized files. Separated mainpy into multiple class files.

---
+++ generator.py
@@ -0,0 +1,12 @@
+from elevator import Elevator
+
+# Generating people
+class Generator():
+    def __init__(self,
+            initFloorRange: list,
+            finalFloorRange: list,
+            targetElevator: Elevator
+        ):
+        self.initFloorRange = initFloorRange
+        self.finalFloorRange = finalFloorRange
+        self.targetElevator = targetElevator
